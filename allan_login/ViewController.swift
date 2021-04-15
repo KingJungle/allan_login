e602d3e07368825729e2997d6ab97923e51e1bd6
allan_joseph <allanjoseph70@gmail.com>
2021-04-15 11:41:12 -0400

    Initial Commit

---
+++ allan_login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  allan_login
+//
+//  Created by Allan Joseph on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
